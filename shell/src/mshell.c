ddc8ab6a2084fcea46cebd02c84c43a5976fd694
lukasaristide <luk2002luk@gmail.com>
2020-11-03 13:20:02 +0100

    no new features, just improved readability

--- shell/src/mshell.c
+++ shell/src/mshell.c
@@ -15,77 +15,44 @@
 #include "utils.h"
 #include "builtins.h"
 
+struct buffers{
+	size_t placeInBuffer;
+	size_t placeInLine;
+	size_t howManyDidIRead;
+	char buf[10 * MAX_LINE_LENGTH + 1];
+	char firstLine[MAX_LINE_LENGTH + 1];
+};
+
 //name says everything
-size_t count_args(command *com){
-	argseq * argseq = com->args;
-	size_t argcounter = 0;
-	while(1){
-		argcounter++;
-		argseq = argseq->next;
-		if(argseq==com->args)
-			break;
-	}
-	return argcounter;
-}
+size_t count_args(command *com);
 
 //copying strings representing args from command struct to array of null-terminated strings
-void copy_args(char ** dest, command * source){
-	argseq * argseq = source->args;
-	size_t i = 0;
-	while(1){
-		dest[i] = malloc(strlen(argseq->arg)+1);
-		strcpy(dest[i++], argseq->arg);
-		argseq = argseq->next;
-			if(argseq==source->args)
-				break;
-	}
-}
+void copy_args(char ** dest, command * source);
 
 //free memory at Argv to prevent memory leaks
-void free_Argv(char ** Argv){
-	size_t i = 0;
-	while(1){
-		if(Argv[i] != NULL)
-			free(Argv[i++]);
-		else
-			break;
-	}
-	free(Argv);
-}
+void free_Argv(char ** Argv);
+
+//name says everything
+bool handle_builtins(char ** Argv);
 
 //run command and handle possible errors
-void execute_process(char ** Argv){
-	if(-1 == execvp(Argv[0], Argv)){
-		int errorno = errno;
-		//write command's text
-		write(2, Argv[0], strlen(Argv[0]));
-		switch(errorno){
-		case ENOENT:
-			//no such file or dir
-			write(2,": no such file or directory\n",28);
-			break;
-		case EACCES:
-			//permission denied
-			write(2,": permission denied\n",20);
-			break;
-		default:
-			//anything else that can possibly go wrong
-			write(2,": exec error\n",13);
-		}
-		exit(EXEC_FAILURE);
-	}
-	exit(EXIT_SUCCESS);
-}
+void execute_process(char ** Argv);
+
+#define GO_END_LOOP 1
+#define GO_BEG_LOOP -1
+#define GO_NORMAL 0
+#define GO_SYNTAX 2
+//this handles buffers, reads from stdin and extracts lines one by one
+//defines above are all the possible return values - they specify, what to do next
+int read_before_parse(struct buffers * buf);
 
 int main(int argc, char *argv[])
 {
 	pipelineseq * ln;
 	command * com;
-	size_t howManyDidIRead = 0;
 	size_t argcounter;
-	size_t placeInBuffer = 0, placeInLine = 0, i;
-	char buf[10 * MAX_LINE_LENGTH + 1];
-	char firstLine[MAX_LINE_LENGTH + 1];
+	struct buffers buf = {0, 0, 0};
+	size_t i;
 	char ** Argv;
 	__pid_t forked;
 	struct stat fstat_buf;
@@ -102,80 +69,22 @@ int main(int argc, char *argv[])
 		if(S_ISCHR(fstat_buf.st_mode))
 			write(1, PROMPT_STR, strlen(PROMPT_STR));
 
-		if(placeInBuffer >= howManyDidIRead){
-			//reading line + memorizing, how many chars were there
-			howManyDidIRead = read(0, buf, 10 * MAX_LINE_LENGTH);
-			placeInBuffer = 0;
-
-			//if eof encountered - stop
-			if(howManyDidIRead <= 0)
-				goto end_main_loop;
-
-			//instead of clearing buffer - let's add zero after read content
-			buf[howManyDidIRead] = 0;
-		}
-
-		//extracting first line
-		for(placeInLine = 0; placeInBuffer < howManyDidIRead; placeInLine++, placeInBuffer++){
-			//if current char is '#' - end line here, but consume rest from the buffer
-			if(buf[placeInBuffer] == '#'){
-				firstLine[placeInLine] = '\n';
-				firstLine[placeInLine+1] = 0;
-				comment = true;
-			}
-
-			//copy current char from buffer to current line
-			//skipped if we're in a comment or we are erasing a line too long to be parsed
-			if(!erasing_now && !comment)
-				firstLine[placeInLine] = buf[placeInBuffer];
-
-			//if endline encountered - finalize extracting line
-			if(buf[placeInBuffer] == '\n'){
-				placeInBuffer++;
-				//we finished a line that is too long - syntax error it is
-				if(erasing_now){
-					erasing_now = false;
-					goto syntax_err;
-				}
-				firstLine[placeInLine+1] = 0;
-				break;
-			} else if(placeInLine == MAX_LINE_LENGTH - 1)
-				erasing_now = true;
-
-			//buf is insufficient - let's move it forward
-			if(placeInBuffer >= howManyDidIRead - 1){
-				//reading line + memorizing, how many chars were there
-				howManyDidIRead = read(0, buf, 10 * MAX_LINE_LENGTH);
-				placeInBuffer = -1;
-
-				//if eof encountered - stop
-				if(howManyDidIRead == 0){
-					firstLine[placeInLine+1] = '\n';
-					firstLine[placeInLine+2] = 0;
-					break;
-				}
-
-				if(howManyDidIRead < 0)
-					goto end_main_loop;
-
-				//instead of clearing buffer - let's add zero after read content
-				buf[howManyDidIRead] = 0;
-			}
-
-		}
-
-		for(i = 0; i < strlen(firstLine); i++){
-			if(!isspace(firstLine[i]))
-				only_white = false;
-		}
-		if(only_white || comment){
-			comment = false;
+		//reading from stdin and extracting first line to buf.firstLine
+		switch(read_before_parse(&buf)){
+		case GO_NORMAL:
+			break;
+		case GO_BEG_LOOP:
 			goto begin_main_loop;
+		case GO_END_LOOP:
+			goto end_main_loop;
+		case GO_SYNTAX:
+			goto syntax_err;
+		default:
+			goto end_main_loop;
 		}
-		only_white = true;
 
 		//parsing input line
-		ln = parseline(firstLine);
+		ln = parseline(buf.firstLine);
 		//seperating first command
 		com = pickfirstcommand(ln);
 
@@ -199,17 +108,8 @@ int main(int argc, char *argv[])
 		copy_args(Argv, com);
 
 		//handle builtins
-		for(i = 0; builtins_table[i].name != NULL; i++){
-			if(strcmp(builtins_table[i].name, Argv[0]) == 0){
-				if(builtins_table[i].fun(Argv)){
-					write(2, "Builtin ", 8);
-					write(2, Argv[0], strlen(Argv[0]));
-					write(2, " error.\n", 8);
-				}
-				free_Argv(Argv);
-				goto begin_main_loop;
-			}
-		}
+		if(handle_builtins(Argv))
+			goto begin_main_loop;
 
 		//starting new process
 		forked = fork();
@@ -235,5 +135,161 @@ int main(int argc, char *argv[])
 	//end yet it doesn't sit well with the tests, so it's commented
 	//write(1,"\n",1);
 
+	exit(EXIT_SUCCESS);
+}
+
+int read_before_parse(struct buffers * buf){
+	bool erasing_now = false;
+	bool only_white = true;
+	bool comment = false;
+	if ((*buf).placeInBuffer >= (*buf).howManyDidIRead) {
+		//reading line + memorizing, how many chars were there
+		(*buf).howManyDidIRead = read(0, (*buf).buf, 10 * MAX_LINE_LENGTH);
+		(*buf).placeInBuffer = 0;
+
+		//if eof encountered - stop
+		if ((*buf).howManyDidIRead <= 0)
+			return GO_END_LOOP;
+
+		//instead of clearing buffer - let's add zero after read content
+		(*buf).buf[(*buf).howManyDidIRead] = 0;
+	}
+
+	//extracting first line
+	for ((*buf).placeInLine = 0; (*buf).placeInBuffer < (*buf).howManyDidIRead;
+			(*buf).placeInLine++, (*buf).placeInBuffer++) {
+		//if current char is '#' - end line here, but consume rest from the buffer
+		if ((*buf).buf[(*buf).placeInBuffer] == '#') {
+			(*buf).firstLine[(*buf).placeInLine] = '\n';
+			(*buf).firstLine[(*buf).placeInLine + 1] = 0;
+			comment = true;
+		}
+
+		//copy current char from buffer to current line
+		//skipped if we're in a comment or we are erasing a line too long to be parsed
+		if (!erasing_now && !comment)
+			(*buf).firstLine[(*buf).placeInLine] =
+					(*buf).buf[(*buf).placeInBuffer];
+
+		//if endline encountered - finalize extracting line
+		if ((*buf).buf[(*buf).placeInBuffer] == '\n') {
+			(*buf).placeInBuffer++;
+			//we finished a line that is too long - syntax error it is
+			if (erasing_now) {
+				erasing_now = false;
+				return GO_SYNTAX;
+			}
+			(*buf).firstLine[(*buf).placeInLine + 1] = 0;
+			break;
+		//apparently, upcoming line is too long - let's start erasing it from the buffer
+		} else if ((*buf).placeInLine == MAX_LINE_LENGTH - 1)
+			erasing_now = true;
+
+		//buf is insufficient - let's move it forward
+		if ((*buf).placeInBuffer >= (*buf).howManyDidIRead - 1) {
+			//reading line + memorizing, how many chars were there
+			(*buf).howManyDidIRead = read(0, (*buf).buf, 10 * MAX_LINE_LENGTH);
+			(*buf).placeInBuffer = -1;
+
+			//if eof encountered - stop
+			if ((*buf).howManyDidIRead == 0) {
+				(*buf).firstLine[(*buf).placeInLine + 1] = '\n';
+				(*buf).firstLine[(*buf).placeInLine + 2] = 0;
+				break;
+			}
+
+			//if read failed - end shell
+			if ((*buf).howManyDidIRead < 0)
+				return GO_END_LOOP;
+
+			//instead of clearing buffer - let's add zero after read content
+			(*buf).buf[(*buf).howManyDidIRead] = 0;
+		}
+
+	}
+
+	for (int i = 0; i < strlen((*buf).firstLine); i++) {
+		if (!isspace((*buf).firstLine[i]))
+			only_white = false;
+	}
+	if (only_white || comment) {
+		comment = false;
+		return GO_BEG_LOOP;
+	}
+	only_white = true;
+	return GO_NORMAL;
+}
+
+size_t count_args(command *com){
+	argseq * argseq = com->args;
+	size_t argcounter = 0;
+	while(1){
+		argcounter++;
+		argseq = argseq->next;
+		if(argseq==com->args)
+			break;
+	}
+	return argcounter;
+}
+
+void copy_args(char ** dest, command * source){
+	argseq * argseq = source->args;
+	size_t i = 0;
+	while(1){
+		dest[i] = malloc(strlen(argseq->arg)+1);
+		strcpy(dest[i++], argseq->arg);
+		argseq = argseq->next;
+			if(argseq==source->args)
+				break;
+	}
+}
+
+void free_Argv(char ** Argv){
+	size_t i = 0;
+	while(1){
+		if(Argv[i] != NULL)
+			free(Argv[i++]);
+		else
+			break;
+	}
+	free(Argv);
+}
+
+//returns true if any builtin has been found (and executed)
+bool handle_builtins(char ** Argv){
+	for(int i = 0; builtins_table[i].name != NULL; i++){
+		if(strcmp(builtins_table[i].name, Argv[0]) == 0){
+			if(builtins_table[i].fun(Argv)){
+				write(2, "Builtin ", 8);
+				write(2, Argv[0], strlen(Argv[0]));
+				write(2, " error.\n", 8);
+			}
+			free_Argv(Argv);
+			return true;
+		}
+	}
+	return false;
+}
+
+void execute_process(char ** Argv){
+	if(-1 == execvp(Argv[0], Argv)){
+		int errorno = errno;
+		//write command's text
+		write(2, Argv[0], strlen(Argv[0]));
+		switch(errorno){
+		case ENOENT:
+			//no such file or dir
+			write(2,": no such file or directory\n",28);
+			break;
+		case EACCES:
+			//permission denied
+			write(2,": permission denied\n",20);
+			break;
+		default:
+			//anything else that can possibly go wrong
+			write(2,": exec error\n",13);
+		}
+		exit(EXEC_FAILURE);
+	}
 	exit(EXIT_SUCCESS);
 }
